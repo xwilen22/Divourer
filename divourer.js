ab81675cc6e1655d3f5d414d637a99708c67cc5e
William <xwilen22@gmail.com>
2020-08-20 19:01:25 +0200

    Possible to hide element at mouse position, toggleable button for 'arming' and 'disarming'

--- divourer.js
+++ divourer.js
@@ -1,2 +1,62 @@
-console.log("Hello?")
-//document.body.style.color = "green"
+(function () {
+    if (window.hasRun) {
+        return
+    }
+    window.hasRun = true
+    
+    const hiddenElements = []
+    let hideOnClickActive = false
+
+    function hideElementAtPosition(x, y) {
+        let retrievedElement = document.elementFromPoint(x, y)
+        retrievedElement.style.display = "none"
+        hiddenElements.push(retrievedElement)
+    }
+
+    function clearAll() {
+        for(element in hiddenElements) {
+            element.style.display = "initial"
+        }
+    }
+    
+    function setHideOnClick(enabledBool) {
+        console.log("Hide on click called ", enabledBool)
+        hideOnClickActive = enabledBool
+    }
+
+    function requestHideOnClick() {
+        return Boolean(hideOnClickActive)
+    }
+
+    function initializeEventListeners() {
+        document.addEventListener("click", (event) => {
+            console.log("CLICK? ", requestHideOnClick())
+            if(!requestHideOnClick()) {
+                return
+            }
+    
+            console.log(`Mouse click: X ${event.clientX}, Y ${event.clientY}`)
+            hideElementAtPosition(event.clientX, event.clientY)
+        })
+    }
+    
+    initializeEventListeners()
+    
+    browser.runtime.onMessage.addListener((message) => {
+        console.log(`Message recieved! ${message.command}`)
+        switch(message.command) {
+            case "enabled" : {
+                setHideOnClick(true)
+                break;
+            }
+            case "disabled" : {
+                setHideOnClick(false)
+                break;
+            }
+            case "reset" : {
+                clearAll()
+                break;
+            }
+        }
+    })
+})()
--- popup/controlpanel.js
+++ popup/controlpanel.js
@@ -1,37 +1,78 @@
 const ID_BUTTON_ARM = "button_toggle_arm"
 const ID_BUTTON_CLEAR = "button_clear"
 
+const POINTER_NAME = "crosshair"
+
+const CSS_DIVOURER_ACTIVE = `
+    html {
+        cursor: ${POINTER_NAME}
+    }
+    a, input {
+        pointer-events:none
+    }
+`
+
 let divourActive = false
 
 function initializeListeners() {
+    console.log("Initialized")
     const armButton = document.getElementById(ID_BUTTON_ARM)
-    armButton.addEventListener("click", (event) => {
-        divourActive = !divourActive
-        
-        armButton.innerText = divourActive ? "Disarm" : "Arm"
-    })
+    armButton.addEventListener("click", onArmButton)
 
     const clearButton = document.getElementById(ID_BUTTON_CLEAR)
-    clearButton.addEventListener("click", (event) => {
-        
-        browser.tabs.query({active: true, currentWindow: true})
-        .then(() => {
-            browser.tabs.removeCSS({code: hidePage}).then(() => {
+    clearButton.addEventListener("click", onClearButton)
+}
+
+function onArmButton(event) {
+    const armButton = document.getElementById(ID_BUTTON_ARM)
+
+    divourActive = !divourActive
+    armButton.innerText = divourActive ? "Disarm" : "Arm"
+    
+    browser.tabs.query({
+        currentWindow: true,
+        active: true
+    })
+    .then((tabs) => {
+        let command = divourActive ? "enabled" : "disabled"
+        if(divourActive) {
+            browser.tabs.insertCSS({code: CSS_DIVOURER_ACTIVE})
+            .then(() => {
                 browser.tabs.sendMessage(tabs[0].id, {
-                    command: "reset",
+                    command
                 })
+                .catch(reportException)
+            })
+            .catch(reportException)
+        } else {
+            browser.tabs.removeCSS({code: CSS_DIVOURER_ACTIVE})
+            .then(() => {
+                browser.tabs.sendMessage(tabs[0].id, {
+                    command
+                })
+                .catch(reportException)
+            })
+            .catch(reportException)
+        }
+    })
+    .catch(reportException);  
+}
+function onClearButton(event) {
+    browser.tabs.query({active: true, currentWindow: true})
+        .then((tabs) => {
+            browser.tabs.sendMessage(tabs[0].id, {
+                command: "reset",
             })
         })
         .catch(reportException);
-    })
 }
-function reportException(exception) {
-    console.log(`ERROR ${exception}`)
-}
-function clearHidden() {
 
+function reportException(exception) {
+    console.log(`BOLLOXED! ${exception} Stack: ${exception.stack}`)
 }
 
 browser.tabs.executeScript({file: "/divourer.js"})
-.then(initializeListeners)
+.then(() => {
+    initializeListeners()
+})
 .catch(reportException)
