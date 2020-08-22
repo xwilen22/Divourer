eac841c9920d5871acb4ced13e1cf53288e92e2f
William <xwilen22@gmail.com>
2020-08-22 19:07:32 +0200

    Added listener for context menu

--- contextMenu.js
+++ contextMenu.js
@@ -1,8 +1,25 @@
-browser.menus.create({
-    id: "menu-fast-hide",
-    title: "Hide Element",
-    icons: {
-        "48":"icons\divour-48.png"
-    },
-    contexts: ["all"]
-})
+const ID_MENU_QUICK_HIDE = "menu-fast-hide"
+
+function initializeContextMenus() {
+    browser.menus.create({
+        id: ID_MENU_QUICK_HIDE,
+        title: "Hide Element",
+        icons: {
+            "48":"icons\divour-48.png"
+        },
+        contexts: ["all"]
+    })
+}
+
+function initializeListeners() {
+    browser.menus.onClicked.addListener((info, tab) => {
+        switch (info.menuItemId) {
+            case ID_MENU_QUICK_HIDE:
+                console.log("INFO: ", info, tab);
+                break;
+        }
+    })
+}
+
+initializeContextMenus()
+initializeListeners()
