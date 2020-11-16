388cc1e6de468a0fd8cf74340e89f519798cea1a
William <xwilen22@gmail.com>
2020-11-16 21:48:23 +0100

    Availability check progress

--- background/availabilityCheck.js
+++ background/availabilityCheck.js
@@ -1,13 +1,29 @@
-browser.tabs.executeScript({file: "../divourer.js"})
-    .then(() => {
-        browser.runtime.sendMessage({
-            command: "set-enabled"
+window.onload = (event) => {
+    if (!browser.tabs.onUpdated.hasListener(onUpdated)) {
+        browser.tabs.onUpdated.addListener(onUpdated)
+    }
+}
+
+function onUpdated(tabId, changeInfo, tabInfo) {
+        browser.tabs.query({
+            currentWindow: true,
+            active: true
         })
-        .catch(onException);
-    })
-.catch((exception) => {
-    browser.runtime.sendMessage({
-        command: "set-disabled"
-    })
-    onException(exception)
-})
+        .then((tabs) => {
+            console.log(`Updated tab on: ${tabId}\nCurrent tab is now: ${tabs[0].id}`)
+
+            browser.runtime.sendMessage({
+                command: "set-enabled"
+            })
+            .catch((exception) => {
+                console.error(exception)
+                browser.tabs.query({
+                    currentWindow: true,
+                    active: true
+                })
+                .then((tabs) => {
+                    browser.browserAction.disable(tabs[0].id)
+                })
+            })
+        })
+}
